42d9606a1ba8db5c0edec2fb1cfe49b7a8f96b8d
Ryan Young <ryoung786@gmail.com>
2012-09-16 06:18:32 -0400

    14 done

--- src/koans/14_destructuring.clj
+++ src/koans/14_destructuring.clj
@@ -3,39 +3,51 @@
    :city "Testerville"
    :state "TX"})
 
+(defn commafy [delim x]
+  (apply str (interpose delim x)))
+
 (meditations
   "Destructuring is an arbiter: it breaks up arguments"
-  (= __ ((fn [[a b]] (str b a))
+  (= ":bar:foo" ((fn [[a b]] (str b a))
          [:foo :bar]))
 
   "Whether in function definitions"
   (= (str "First comes love, "
           "then comes marriage, "
           "then comes Clojure with the baby carriage")
-     ((fn [[a b c]] __)
+     ((fn [[a b c]]
+        (format "First comes %s, then comes %s, then comes %s with the baby carriage"
+                a b c))
       ["love" "marriage" "Clojure"]))
 
   "Or in let expressions"
   (= "Rich Hickey aka The Clojurer aka Go Time aka Macro Killah"
      (let [[first-name last-name & aliases]
            (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
-       __))
+       (commafy " aka " (cons (str first-name " " last-name) aliases))))
 
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Steven" "Hawking"] :named-parts {:first "Steven" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Steven" "Hawking"]]
-       __))
+       {:original-parts full-name
+        :named-parts {:first first-name :last last-name}
+        }))
 
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
      (let [{street-address :street-address, city :city, state :state} test-address]
-       __))
+       (commafy ", " [street-address city state])))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-       __))
+     (let [{:keys [street-address city state]} test-address]
+       (commafy ", " [street-address city state])))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address)))
+     ((fn [names address]
+       (let [[fname lname] names
+             name (str fname " " lname)
+             {:keys [street-address city state]} address]
+         (commafy ", " [name street-address city state])))
+      ["Test" "Testerson"] test-address)))
